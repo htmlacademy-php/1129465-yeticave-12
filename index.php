fa89b01732a2a4a1d36e73c2c113d2df594ba1f4
Max <maks.kozyr.88@mail.ru>
2021-01-25 20:05:07 +0300

    Выполнил измененения в задании (Работем с условиями)

--- index.php
+++ index.php
@@ -27,14 +27,14 @@ $user_name = "Maksim"; //
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-            <?php if ($is_auth == 1): ?>
+            <?php if ($is_auth === 1): ?>
             <div class="user-menu__logged">
                 <p><?=$user_name;?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-            <?php elseif ($is_auth == 0): ?>
+            <?php else: ?>
             <ul class="user-menu__list">
                 <li class="user-menu__item">
                     <a href="#">Регистрация</a>
